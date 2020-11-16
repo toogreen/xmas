1ef8bb9e87062e1dad68e02e7c30d09dee89a2a2
David Gagnon <toogreen@15inch.lan>
2020-11-16 15:24:47 -0500

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import $ from "jquery";
 import Card from "./Card";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        {/* <Card /> */}
-      </div>
+      <Card />
     </div>
   );
 }
