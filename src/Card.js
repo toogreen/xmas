1418292263b791e739f962d28e86e4a98abbc1b8
David Gagnon <toogreen@15inch.local>
2020-12-12 21:03:13 -0500

    still just trying to get the card to display after converting the code to React

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./css/iphone.css";
 import "./css/main.css";
 import $ from "jquery";
+import MainScript from "./MainScript"
 // import Flocons from "./Flocons";
 
 function Card() {
@@ -72,6 +73,109 @@ function Card() {
                 {/* <!-- Ajouté ça pour meilleures animations --> */}
                 <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
                 
+    // Disable scrolling
+    $("body").css("overflow", "hidden");
+
+    // loader la musique d'avance
+    var audioElement1 = document.createElement('audio');
+
+    audioElement1.setAttribute('src', 'sound/jazzy1.mp3'); // Fonctionne juste sous webkit browsers
+
+    if ($.browser.mozilla || $.browser.opera) {
+        audioElement1.setAttribute('src', 'sound/jazzy1.theora.ogv') // Pour que ça joue partout
+    }
+
+    // Vu que la musique joue pas encore on definie une variable qui servira pour notre bouton play/pause plus tard
+    const audioplaying = false;
+
+
+// D'abord on fait apparaitre etoile qui "glow" pour attirer le clic
+glow1();
+
+    // Si on clique sur l'etoile
+    $("#star").click(function(){
+
+        // On fait apparaitre les lumieres dans l'arbre qui clignotent
+        glow2()
+
+        // Empecher qu'on puisse cliquer a nouveau dessus
+        $(this).unbind('click')
+
+        // Pars la musique!
+        audioElement1.play();
+
+        // On averti notre variable qu'on joue maintenant
+        audioplaying = true;
+
+        // On affiche les boutons pause et reload
+        $("#pause").css({'display':'block', 'opacity':'1'})
+        $("#reload").css({'display':'block', 'opacity':'1'})
+
+
+        // On joue la première séquence (cadeau 1 apparait)
+        animation1();
+
+    });
+
+    // If we click on cadeau1
+    $("#cadeau-open, #cadeau-top").click(function(){
+
+        // Empecher qu'on puisse cliquer a nouveau dessus
+        $(this).unbind('click')
+
+        // Joue animation pour ouvrir cadeau
+        animation2();
+
+    });
+
+    // If we click on message softvoyage
+    $("#message").click(function(){
+
+        // STOP la musique!
+        audioElement1.pause();
+        audioplaying = false;
+        $("#pause").css('background', 'url(img/sound-off.png)');
+
+        // On joue la dernière séquence (Message de bons voeux apparait et l'étoile sur le sapin)
+        animation4();
+
+    });
+
+    // If we click sur bouton pause musique
+    $("#pause").click(function(){
+
+        if(audioplaying == true){
+                    
+            // STOP la musique!
+            audioElement1.pause();
+            audioplaying = false;
+            $("#pause").css('background', 'url(img/sound-off.png)');
+
+        } else {
+            // PLAY la musique!
+            audioElement1.play();
+            audioplaying = true;
+            $("#pause").css('background', 'url(img/sound-on.png)');
+        }
+    });
+
+
+    // Pour les mouse-over sur le bouton reload
+    $( "#reload" ).mouseenter(function() {
+    $(this).css('background', 'url(img/reload-hover.png)');
+    });
+
+    $( "#reload" ).mouseleave(function() {
+    $(this).css('background', 'url(img/reload.png)');
+    });
+
+    // Si on clique sur le bouton reload on reload la page
+    $( "#reload" ).click(function() {
+    location.reload();
+    });
+
+}); // fin du jQuery
+
 
             <MainScript />
 
