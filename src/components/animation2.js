6af130af5f6edc6511dfedd3501b0f829e419445
David Gagnon <toogreen@gmail.com>
2020-12-14 21:38:42 -0500

    just tried a few things didn't make much changes

--- src/components/animation2.js
+++ src/components/animation2.js
@@ -6,7 +6,7 @@ function animation2() {
         $("#cadeau-top").css({'display':'block', 'opacity':'1'}).animate({bottom:"+=100px"},1000);
 
         // L'avion apparait en fade in
-        $("#paperplane" ).css({'display':'block', 'opacity':'0'}).fadeTo( 1000 , 1, function() {animation3()});
+        //$("#paperplane" ).css({'display':'block', 'opacity':'0'}).fadeTo( 1000 , 1, function() {animation3()});
 
 }
 
